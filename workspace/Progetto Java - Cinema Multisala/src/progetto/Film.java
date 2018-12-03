ffc457b1088d8501e377aa0a873ca300817dfae4
raffycaccia <45554975+raffycaccia@users.noreply.github.com>
2018-12-03 17:49:37 +0100

    commenti FIlm.java

--- workspace/Progetto Java - Cinema Multisala/src/progetto/Film.java
+++ workspace/Progetto Java - Cinema Multisala/src/progetto/Film.java
@@ -3,15 +3,25 @@ package progetto;
 public class Film {
 	String titolo, desc;
 	
+	/*
+	 * Costruttore, inizializza le variabili del Film
+	 */
 	Film(String titolo, String desc){
 		this.titolo=titolo;
 		this.desc=desc;
 	}
 	
+	
+	/*
+	 * ritorna il titolo del Film
+	 */
 	public String getTitolo() {
 		return titolo;
 	}
 	
+	/*
+	 * ritorna la descrizione del Film
+	 */
 	public String GetDescrizione() {
 		return desc;
 	}
