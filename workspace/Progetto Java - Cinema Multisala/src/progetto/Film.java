6164343771c33c893ca4f6f432d029201f0efed1
raffycaccia <45554975+raffycaccia@users.noreply.github.com>
2018-12-03 17:45:08 +0100

    classe Film

---
+++ workspace/Progetto Java - Cinema Multisala/src/progetto/Film.java
@@ -0,0 +1,19 @@
+package progetto;
+
+public class Film {
+	String titolo, desc;
+	
+	Film(String titolo, String desc){
+		this.titolo=titolo;
+		this.desc=desc;
+	}
+	
+	public String getTitolo() {
+		return titolo;
+	}
+	
+	public String GetDescrizione() {
+		return desc;
+	}
+	
+}
