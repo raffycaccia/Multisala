8427815cabc04a3fb60576e9940e99809f725a07
Gianluca <kigampixels@me.com>
2018-12-03 22:27:15 +0100

    Prima non avevo salvato lol

--- workspace/Progetto Java - Cinema Multisala/src/progetto/PoliticaSconto.java
+++ workspace/Progetto Java - Cinema Multisala/src/progetto/PoliticaSconto.java
@@ -2,17 +2,31 @@ package progetto;
 
 public class PoliticaSconto {
 	
+	//Dichiarazione variabili d'istanza
 	private float percentuale;
 	private boolean politicaAttiva;
 	
+	
+	//Costruttore
+	/**
+	 	Costruisce una politica di sconto con uno stato
+	 	e una determinata percentuale di sconto.
+	 	@param stato: Indica se la politica é attiva o meno
+	 	@param percentuale: La percentuale di sconto di questa politica
+	 */
 	public PoliticaSconto(boolean stato, float percentuale) {
 		this.politicaAttiva = stato;
 		this.percentuale = percentuale;
 	}
-
-
+	
+	//Metodi modificatori
+	
 	public void setPoliticaSconto(boolean stato, float percentuale) {
 		this.politicaAttiva = stato;
+		this.percentuale = percentuale;
+	}
+
+	public void setPercentualeSconto(float percentuale) {
 		this.percentuale = percentuale;
 	}
 	
@@ -20,17 +34,18 @@ public class PoliticaSconto {
 		this.politicaAttiva = true;
 	}
 	
-	
 	public void setDisabled() {
 		this.politicaAttiva = false;
 	}
 	
-	public void setPercentualeSconto(float percentuale) {
-		this.percentuale = percentuale;
-	}
+	//Metodi d'accesso
 	
 	public float getPercentualeSconto() {
-		return this.percentuale;
+		return percentuale;
+	}
+	
+	public boolean getStato() {
+		return politicaAttiva;
 	}
 
 }
