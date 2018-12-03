e1c823f8ac6f2467464d55805d8a010e0d4ddc7c
raffycaccia <45554975+raffycaccia@users.noreply.github.com>
2018-12-03 20:41:19 +0100

    Aggiornato Film.Java
    Aggiunti i commenti utilizzando @param e @return; aggiunti metodi set

--- workspace/Progetto Java - Cinema Multisala/src/progetto/Film.java
+++ workspace/Progetto Java - Cinema Multisala/src/progetto/Film.java
@@ -5,6 +5,8 @@ public class Film {
 	
 	/*
 	 * Costruttore, inizializza le variabili del Film
+	 * @param titolo il titolo del film
+	 * @param desc descrizione del film
 	 */
 	Film(String titolo, String desc){
 		this.titolo=titolo;
@@ -13,17 +15,36 @@ public class Film {
 	
 	
 	/*
-	 * ritorna il titolo del Film
+	 * Questo metodo ritorna il titolo del film
+	 * @return String
 	 */
 	public String getTitolo() {
 		return titolo;
 	}
 	
 	/*
-	 * ritorna la descrizione del Film
+	 * Questo metodo ritorna la descrizione del film
+	 * @return String
 	 */
 	public String GetDescrizione() {
 		return desc;
+	}
+	
+	
+	/*
+	 * Questo metodo modifica il titolo del film
+	 * @param newtitolo il nuovo titolo del film
+	 */
+	public void setTitolo(String newtitolo) {
+		titolo=newtitolo;
+	}
+	
+	/*
+	 * Questo metodo modifica la descrizione del film
+	 * @param newtitolo la nuova descrizione del film
+	 */
+	public void setDescrizione(String newDesc) {
+		desc=newDesc;
 	}
 	
 }
