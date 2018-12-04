7590b98e1a71614c3ae3e0a581769d3723caa762
Gianluca <kigampixels@me.com>
2018-12-04 01:03:53 +0100

    (Struttura e Commenti) - Aggiunta cartella guide, aggiunti commenti su PoliticaSconto

--- workspace/Progetto Java - Cinema Multisala/src/progetto/PoliticaSconto.java
+++ workspace/Progetto Java - Cinema Multisala/src/progetto/PoliticaSconto.java
@@ -3,16 +3,18 @@ package progetto;
 public class PoliticaSconto {
 	
 	//Dichiarazione variabili d'istanza
+	
 	private float percentuale;
 	private boolean politicaAttiva;
 	
 	
 	//Costruttore
+	
 	/**
 	 	Costruisce una politica di sconto con uno stato
 	 	e una determinata percentuale di sconto.
-	 	@param stato: Indica se la politica é attiva o meno
-	 	@param percentuale: La percentuale di sconto di questa politica
+	 	@param stato		indica se la politica é attiva o meno
+	 	@param percentuale 	la percentuale di sconto di questa politica
 	 */
 	public PoliticaSconto(boolean stato, float percentuale) {
 		this.politicaAttiva = stato;
@@ -21,29 +23,53 @@ public class PoliticaSconto {
 	
 	//Metodi modificatori
 	
+	/**
+	 	Modifica una politica di sconto con un nuovo stato
+	 	e una determinata percentuale di sconto.
+	 	@param stato		indica se la politica é attiva o meno
+	 	@param percentuale 	la percentuale di sconto di questa politica
+	 */
 	public void setPoliticaSconto(boolean stato, float percentuale) {
 		this.politicaAttiva = stato;
 		this.percentuale = percentuale;
 	}
 
+	/**
+	 * Modifica la percentuale di sconto.
+	 * @param percentuale 	la percentuale di sconto di questa politica
+	 */
 	public void setPercentualeSconto(float percentuale) {
 		this.percentuale = percentuale;
 	}
 	
+	/**
+	 * Attiva la politica di sconto.
+	 */
 	public void setActive() {
 		this.politicaAttiva = true;
 	}
 	
+	/**
+	 * Disabilita la politica di sconto.
+	 */
 	public void setDisabled() {
 		this.politicaAttiva = false;
 	}
 	
 	//Metodi d'accesso
 	
+	/**
+	 * Restituisce la percentuale di sconto della politica.
+	 * @return la percentuale di sconto
+	 */
 	public float getPercentualeSconto() {
 		return percentuale;
 	}
 	
+	/**
+	 * Restituisce lo stato di validita della politica.
+	 * @return lo stato di validita della politica di sconto
+	 */
 	public boolean getStato() {
 		return politicaAttiva;
 	}
