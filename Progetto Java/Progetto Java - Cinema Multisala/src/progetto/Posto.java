2bf2d8762b13aab4530afedd9a48b273ea47dbbb
raffycaccia <45554975+raffycaccia@users.noreply.github.com>
2018-12-05 18:09:05 +0100

    VARI FILE - leggi la descrizione
    Finito il file Sala.java //vanno rifatti tutti i vari commenti
    Quasi finito il file Posti.java mancanoa alcune correzzioni

--- Progetto Java/Progetto Java - Cinema Multisala/src/progetto/Posto.java
+++ Progetto Java/Progetto Java - Cinema Multisala/src/progetto/Posto.java
@@ -1,25 +1,91 @@
 package progetto;
 
-import javax.swing.JComponent;
+import javax.swing.JButton;
 
 public class Posto extends JButton{
+	//TODO si devono rifare tutti i commenti
+	int id;
+	private boolean disponibile,indisponibile,prenotato,venduto;
 
+	Posto(int id){
+		this.id=id;
+	}
+
+	//metodi di get
+	
 	/**
-	 * stati:
-	 * 	utilizzabili
-	 * 	indisponibili
-	 * 	prenotati
-	 * 	venduti
+	 * 
+	 * @return
 	 */
-	int status;
-	
-	
-	Posto(){
-		
+	public int getId() {
+		return id;
 	}
 	
-	public int getStatus() {
-		return status;
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isDisponibile() {
+		return disponibile;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isIndisponibile() {
+		return indisponibile;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isPrenotato() {
+		return prenotato;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isVenduto() {
+		return venduto;
+	}
+
+
+	//metodi di set
+
+	/**
+	 * accetta valori true e false 
+	 * @param disp
+	 */
+	public void setDisponibile(boolean disp) {
+		this.disponibile=disp;
+	}
+
+	/**
+	 * accetta valori true e false 
+	 * @param 
+	 */
+	public void setIndisponibile(boolean indisp) {
+		this.indisponibile=indisp;
+	}
+
+	/**
+	 * accetta valori true e false 
+	 * @param 
+	 */
+	public void setPrenotato(boolean pren) {
+		this.prenotato=pren;
+	}
+
+	/**
+	 * accetta valori true e false 
+	 * @param 
+	 */
+	public void setVenduto(boolean vend) {
+		this.venduto=vend;
 	}
 	
 }
