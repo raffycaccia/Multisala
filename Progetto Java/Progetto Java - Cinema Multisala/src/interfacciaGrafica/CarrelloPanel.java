57e6bdacc4f79895660d6ce76dc62c8fd2f20474
raffycaccia <45554975+raffycaccia@users.noreply.github.com>
2018-12-06 16:53:13 +0100

    GESTIONE CARRELLO

--- Progetto Java/Progetto Java - Cinema Multisala/src/interfacciaGrafica/CarrelloPanel.java
+++ Progetto Java/Progetto Java - Cinema Multisala/src/interfacciaGrafica/CarrelloPanel.java
@@ -1,38 +1,84 @@
 package interfacciaGrafica;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import eccezioni.PostoIndisponibileException;
+import progetto.Posto;
 
 public class CarrelloPanel extends JPanel{
 	JPanel cart_element = new JPanel();
+	ArrayList<PoltroneInCarrello> poltrone = new ArrayList<>();
 
 	public CarrelloPanel() {
 		initGui();
 	}
 
 	private void initGui() {
+		
 		this.removeAll();
 		this.setLayout(new BorderLayout(20, 30)); 
 
 		JLabel cartimage = new JLabel(new ImageIcon("image/carrello/immagine.png"));
-		JLabel button_buy = new JLabel(new ImageIcon("image/carrello/pulsante.png"));
+		JButton button_buy = new JButton(new ImageIcon("image/carrello/pulsante.png"));
 
 		this.add(cart_element,BorderLayout.CENTER);
 		this.add(cartimage,BorderLayout.NORTH);
 		this.add(button_buy,BorderLayout.SOUTH);
-
+		
+		button_buy.addActionListener(AcquistaClick());
+		button_buy.setBorderPainted(false);
+		button_buy.setContentAreaFilled(false);
+		
 		this.setVisible(true);
 	}
-
-	public void addPostoCarrello() {
-
+	
+	/**
+	 * metodo che aggiorna il carrello
+	 */
+	private void refresh() {
+		cart_element.removeAll();
+		for(int i=0; i<poltrone.size();i++) {
+			cart_element.add(poltrone.get(i));
+		}
 	}
 	
+	/**
+	 * Metodo che aggiunge una poltrona al carrello
+	 */
+	public void addPostoCarrello(Posto posto) {
+		poltrone.add(new PoltroneInCarrello(posto));
+		refresh();
+	}
+	
+	/**
+	 * Metodo che rimuove una poltrona al carrello
+	 * 	qui va fatta anche la ricercaa!!
+	 */
 	public void removePostoCarrello() {
-
+		//ciclo
+		refresh();
+	}
+	
+	
+	private ActionListener  AcquistaClick() {
+		ActionListener evento = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "PROVA EVENTO");
+			}
+				
+			};
+		
+		return evento;
 	}
 
 }
---
+++ Progetto Java/Progetto Java - Cinema Multisala/src/interfacciaGrafica/PoltroneInCarrello.java
@@ -0,0 +1,16 @@
+package interfacciaGrafica;
+
+import javax.swing.JPanel;
+
+import progetto.Posto;
+
+public class PoltroneInCarrello extends JPanel{
+
+	public PoltroneInCarrello(Posto posto) {
+		initGui();
+	}
+	
+	private void initGui() {
+		
+	}
+}
