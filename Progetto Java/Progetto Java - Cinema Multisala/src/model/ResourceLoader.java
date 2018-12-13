9ce393e34ce279e66ac36eaf863dd684e6a02ae1
Gianluca <kigampixels@me.com>
2018-12-13 14:53:01 +0100

    conflitto risolto

--- Progetto Java/Progetto Java - Cinema Multisala/src/model/ResourceLoader.java
+++ Progetto Java/Progetto Java - Cinema Multisala/src/model/ResourceLoader.java
@@ -2,25 +2,19 @@ package model;
 
 import java.awt.Font;
 import java.awt.FontFormatException;
-import java.awt.Image;
-//import java.awt.image.BaseMultiResolutionImage;
-//import java.awt.image.MultiResolutionImage;
+
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
 
 public class ResourceLoader {
 	
 
-	public static Font TitleFont;
-	public static Font DescFont;
-	public static Font HeaderFont;
-	public static Font InfoFont;
-	public static Font DataFont;
-	public static Font BtnFont;
-	public static Font BtnSelFont;
+	public static Font HeavyFont;
+	public static Font BookObliqueFont;
+	public static Font BlackFont;
+	public static Font BookFont;
+	public static Font MediumFont;
+	//public static MultiResolutionImage clienteBtnIcon;
 	
 	/* Momentaneamente opzionale
 	public ResourceLoader() {
@@ -30,13 +24,11 @@ public class ResourceLoader {
 	
 	public static void loadFonts() {
 		try {
-			DescFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-BookOblique.ttf")).deriveFont(16f));
-			TitleFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Heavy.ttf")).deriveFont(28f));
-			HeaderFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Black.ttf")).deriveFont(45f));
-			InfoFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Heavy.ttf")).deriveFont(18f));
-			DataFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Heavy.ttf")).deriveFont(18f));
-			BtnFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Book.ttf")).deriveFont(19f));
-			BtnSelFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Medium.ttf")).deriveFont(19f));
+			BookObliqueFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-BookOblique.ttf")));
+			HeavyFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Heavy.ttf")));
+			BlackFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Black.ttf")));
+			BookFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Book.ttf")));
+			MediumFont = (Font.createFont(Font.TRUETYPE_FONT, new File("./Fonts/Avenir-Medium.ttf")));
 		} catch (FontFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -45,12 +37,13 @@ public class ResourceLoader {
 			e.printStackTrace();
 		}
 	}
-	
+	/*
 	public static void loadButtonsIcon() throws IOException {
 		ArrayList<Image> imgList = new ArrayList<Image>();
 		imgList.add(ImageIO.read(new File("Cliente.png")));
 		imgList.add(ImageIO.read(new File("Cliente@2x.png")));
 		imgList.add(ImageIO.read(new File("Cliente@3x.png")));
+		clienteBtnIcon = new BaseMultiResolutionImage(imgList.toArray(new Image[0]));
 	}
-	
+	*/
 }
